bfd3856e784f1242015f6bbf883db6037d4743f7
Jonathan Traverso <jtraverso7@gmail.com>
2020-01-25 13:22:20 -0500

    Work in progress.

--- src/oop/oop1.py
+++ src/oop/oop1.py
@@ -17,3 +17,24 @@
 #     pass
 #
 # Put a comment noting which class is the base class
+
+
+# BELOW WILL BE THE BASE CLASS 'VEHICLE'
+class Vehicle:
+    def __init__(self, name):
+        self.name = name
+
+
+class GroundVehicle(Vehicle):
+    def __init__(self, name):
+        # self.num_wheels = num_wheels
+        # self.drive = drive
+        super.__init__(self, name)
+
+    # def drive(self):
+    #     return "vroooom"
+
+
+class Motorcycle(GroundVehicle):
+    def __init__(self, name):
+        super.__init__(self, name)
